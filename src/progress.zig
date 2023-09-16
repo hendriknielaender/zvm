14cfe927fd97ad6c85354ab9e5067fdcdd551d9e
Hendrik Nieländer <hendrik@nielaender.de>
2023-09-16 14:58:40 +0300

    chore: improved progress print

--- src/progress.zig
+++ src/progress.zig
@@ -1,24 +1,49 @@
-// progress.zig
 const std = @import("std");
 
 const ProgressBarLength = 20;
 
 pub fn print(current: usize, total: usize) void {
-    const true_percentage = ((current * 100) / total);
-    const filledBlocks = (true_percentage * ProgressBarLength) / 100;
+    // Ensure bounds are not exceeded
+    if (current > total) return;
 
-    std.debug.print("[", .{});
+    const percentage = (current * 100) / total;
+    const filledBlocks = (percentage * ProgressBarLength) / 100;
 
-    var i: usize = 0;
-    while (i < filledBlocks) {
-        std.debug.print("=", .{});
-        i += 1;
+    // Use a fixed-size buffer
+    var output: [ProgressBarLength + 10]u8 = undefined;
+
+    var output_slice: []u8 = output[0..];
+    var writer = ArrayWriter{ .buf = output[0..], .index = 0 };
+
+    writer.writeByte('[') catch unreachable;
+
+    for (0..filledBlocks) |_| {
+        writer.writeByte('=') catch unreachable;
     }
-    while (i < ProgressBarLength) {
-        std.debug.print(" ", .{});
-        i += 1;
+
+    for (filledBlocks..ProgressBarLength) |_| {
+        writer.writeByte(' ') catch unreachable;
     }
 
-    std.debug.print("] {d}%", .{true_percentage});
-    std.debug.print("\r", .{}); // Move the cursor to the start of the line to overwrite the existing progress bar
+    writer.print("] {d}%\r", .{percentage}) catch unreachable;
+    std.debug.print("{s}\r", .{output_slice[0..writer.index]});
 }
+
+const ArrayWriter = struct {
+    buf: []u8,
+    index: usize,
+
+    pub fn writeByte(self: *ArrayWriter, byte: u8) !void {
+        if (self.index < self.buf.len) {
+            self.buf[self.index] = byte;
+            self.index += 1;
+        } else {
+            return error.NoSpaceLeft;
+        }
+    }
+
+    pub fn print(self: *ArrayWriter, comptime format: []const u8, args: anytype) !void {
+        const bytes_written = (try std.fmt.bufPrint(self.buf[self.index..], format, args)).len;
+        self.index += bytes_written;
+    }
+};
