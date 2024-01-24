979a565daba5d470a63b8fe0114cc05bf7e3a752
Hendrik Nieländer <hendrik@nielaender.de>
2024-01-24 22:09:32 +0100

    refactor(build): optimize cross-compilation targets in build.zig

--- build.zig
+++ build.zig
@@ -1,75 +1,52 @@
 const std = @import("std");
-const builtin = @import("builtin");
-
+const CrossTargetInfo = struct {
+    crossTarget: std.zig.CrossTarget,
+    name: []const u8,
+};
+// Semantic version of your application
 const version = std.SemanticVersion{ .major = 0, .minor = 1, .patch = 2 };
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    // Add a global option for versioning
     const options = b.addOptions();
     options.addOption(std.SemanticVersion, "zvm_version", version);
 
-    const exe = b.addExecutable(.{
-        .name = "zvm",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-        .version = version,
-    });
-    exe.linkLibC();
+    const crossTargets = [_]CrossTargetInfo{
+        CrossTargetInfo{ .crossTarget = std.zig.CrossTarget{ .cpu_arch = .x86_64, .os_tag = .macos, .abi = .musl }, .name = "zvm_macos-x86_64-musl" },
+        CrossTargetInfo{ .crossTarget = std.zig.CrossTarget{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl }, .name = "zvm_linux-x86_64-musl" },
+        // Add more targets as necessary
+    };
 
-    exe.addIncludePath(.{ .path = "src/deps/libarchive/libarchive" });
-    exe.addLibraryPath(.{ .path = "src/deps" });
-    exe.addLibraryPath(.{ .path = "/usr/lib/x86_64-linux-gnu" });
-    exe.addLibraryPath(.{ .path = "/usr/local/lib" });
-    exe.linkSystemLibrary("archive"); // libarchive
-    exe.linkSystemLibrary("lzma"); // liblzma
+    // Function to create executables for each target
+    for (crossTargets) |targetInfo| {
+        const exe = b.addExecutable(.{
+            .name = targetInfo.name,
+            .root_source_file = .{ .path = "src/main.zig" },
+            .target = targetInfo.crossTarget,
+            .optimize = optimize,
+            .version = version,
+        });
 
-    exe.addOptions("options", options);
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
+        // Link dependencies and set include paths
+        exe.linkLibC();
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+        exe.addIncludePath(.{ .path = "src/deps/libarchive/libarchive" });
+        exe.addLibraryPath(.{ .path = "src/deps" });
+        exe.addLibraryPath(.{ .path = "/usr/lib/x86_64-linux-gnu" });
+        exe.addLibraryPath(.{ .path = "/usr/local/lib" });
+        exe.linkSystemLibrary("archive"); // libarchive
+        exe.linkSystemLibrary("lzma"); // liblzma
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
+        exe.addOptions("options", options);
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        b.installArtifact(exe);
     }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
+    const target = b.standardTargetOptions(.{});
     const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
@@ -83,4 +60,6 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
+    // Additional build steps for different configurations or tasks
+    // Add here as needed (e.g., documentation generation, code linting)
 }
