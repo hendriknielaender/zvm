01395c3e5f7b2c241f2b294acdb9dcfd90946f66
hndrk <51416554+hendriknielaender@users.noreply.github.com>
2025-03-05 18:50:11 +0100

    chore: bump version to 0.9.0

--- build.zig
+++ build.zig
@@ -4,8 +4,8 @@ const builtin = @import("builtin");
 const Build = std.Build;
 
 const min_zig_string = "0.14.0";
-const semver = std.SemanticVersion{ .major = 0, .minor = 8, .patch = 1 };
-const semver_string = "0.8.1";
+const semver = std.SemanticVersion{ .major = 0, .minor = 9, .patch = 0 };
+const semver_string = "0.9.0";
 
 const CrossTargetInfo = struct {
     crossTarget: std.zig.CrossTarget,
